d4f9caeb538769c4b0df83577e4694cc44de8601
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2024-07-05 23:44:33 -0300

    feat: adicionando o express e conseguindo trazer os dados em questão

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
 
-(async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({headless:false});
+const app = express();
+const port = 3050;
+
+app.get('/scrape', async(req, res) => {
+    let elements;
+
+    try{
+        const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
   
     // Navigate the page to a URL
     await page.goto('https://www.duolingo.com/profile/JUBILEU.P', {waitUntil: 'networkidle2'});
 
-    await page.waitForSelector('.-TMd4._1CL8A')
+    await page.waitForSelector('.-TMd4')
 
-    const resultado = await page.evaluate(() => {
-        const element = document.querySelector('.-TMd4._1CL8A');
-        return element ? element.innerText : null;
+    const elements = await page.evaluate(() => {
+       return Array.from(document.querySelectorAll('.-TMd4')).map(el => el.innerText);
     });
-    
-    console.log(resultado);
-  })();
+    }catch(error){
+        console.error('Error scraping:', error);
+        return res.status(500).json({ error: 'Failed to scrape the data' });
+    }
+    res.json({ elements });
+});
+app.listen(port, () =>{
+    console.log(`Server running at http://localhost:${port}/`);
+});
